590590270413fb1b3ddcf9a9ebeae580d42ffa95
Akash kumar Nayak <akashnayak3009@gmail.com>
2023-12-03 10:52:22 +0530

    pagination

--- controllers/productController.js
+++ controllers/productController.js
@@ -80,17 +80,50 @@ export const getAProduct = asyncHandler(async (req, res) => {
 
 // Get all products
 export const getAllProduct = asyncHandler(async (req, res) => {
-    try { 
-        // Find all products
-        const getAllProduct = await Product.find();
-        // Return a success message with all products
-        return res.status(200).json({ status: true, message: "Products fetch successfully", getAllProduct })
+    try {
+      // Filtering
+      const queryObj = { ...req.query };
+      const excludeFields = ["page", "sort", "limit", "fields"];
+      excludeFields.forEach((el) => delete queryObj[el]);
+      let queryStr = JSON.stringify(queryObj);
+      queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+  
+      let query = Product.find(JSON.parse(queryStr));
+  
+      // Sorting
+  
+      if (req.query.sort) {
+        const sortBy = req.query.sort.split(",").join(" ");
+        query = query.sort(sortBy);
+      } else {
+        query = query.sort("-createdAt");
+      }
+  
+      // limiting the fields
+  
+      if (req.query.fields) {
+        const fields = req.query.fields.split(",").join(" ");
+        query = query.select(fields);
+      } else {
+        query = query.select("-__v");
+      }
+  
+      // pagination
+  
+      const page = req.query.page;
+      const limit = req.query.limit;
+      const skip = (page - 1) * limit;
+      query = query.skip(skip).limit(limit);
+      if (req.query.page) {
+        const productCount = await Product.countDocuments();
+        if (skip >= productCount) throw new Error("This Page does not exists");
+      }
+      const product = await query;
+      res.json(product);
     } catch (error) {
-        console.log("Error fetching the all products", error);
-        // Return an error message
-        return res.status(500).json({ status: false, message: "Error fetching the all products" })
+      throw new Error(error);
     }
-})
+  });
 
 
 export const deleteProduct = asyncHandler(async (req, res) => {
